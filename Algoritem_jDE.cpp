e3f5c48f48c1d3e8fb25ca6b6504b96cf26cc5d3
N1ckProgramming <161765353+NikolaPopovski1@users.noreply.github.com>
2024-10-14 20:02:35 +0200

    Initialisation added
    Initialisation almost complete, only energy calculations are missing

--- Algoritem_jDE.cpp
+++ Algoritem_jDE.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
+#include <utility>
+
+// Constants
+const double PI = 3.141592653589793;
+const double XjL = -PI;
+const double XjU = PI;
+const double epsilon = 1e-6;
+
+struct Solution {
+    std::vector<double> xBetas;
+    std::vector<double> xFis;
+    double energy;
+    double F;
+    double Cr;
+};
+
+double energy1Calculation(double fi) {
+
+}
+
+
 
 int main(int argc, char* argv[]) {
+    std::string aminoacids = "";
     unsigned int seed = 0;
     double target = 0.0;
     unsigned int nfesLmt = 0;
     unsigned int runtimeLmt = 0;
-    unsigned int Np = 0;
+    unsigned int np = 0;
 
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
 
-        if ((arg == "A" || arg == "B") && i + 1 < argc) {
-            seed = std::stoi(argv[++i]);
+        if ((arg[0] == 'A' || arg[0] == 'B')) {
+            aminoacids = argv[i];
         }
         else if (arg == "-seed" && i + 1 < argc) {
             seed = std::stoi(argv[++i]);
@@ -28,24 +51,42 @@ int main(int argc, char* argv[]) {
             runtimeLmt = std::stoi(argv[++i]);
         }
         else if (arg == "-Np" && i + 1 < argc) {
-            Np = std::stoi(argv[++i]);
+            np = std::stoi(argv[++i]);
         }
     }
 
-    // Initialisation
-
-
     // Print parsed values to check
-    std::cout << "Seed: " << seed << std::endl;
-    std::cout << "Target: " << target << std::endl;
-    std::cout << "NfesLmt: " << nfesLmt << std::endl;
-    std::cout << "RuntimeLmt: " << runtimeLmt << std::endl;
-    std::cout << "Np: " << Np << std::endl;
+    std::cout << "Input values:" << std::endl;
+    std::cout << "\tAminoacids: " << aminoacids << std::endl;
+    std::cout << "\tSeed: " << seed << std::endl;
+    std::cout << "\tTarget: " << target << std::endl;
+    std::cout << "\tNfesLmt: " << nfesLmt << std::endl;
+    std::cout << "\tRuntimeLmt: " << runtimeLmt << std::endl;
+    std::cout << "\tNp: " << np << std::endl;
 
     // Example usage of seed (set the random seed)
     srand(seed);
 
-    std::cout << "Random number (multiplied by 33): " << rand() * 33 << std::endl;
+    // Initialisation
+    std::vector<Solution> population;
+    for (int i = 0; i < np; i++) {
+        Solution sol;
+        sol.F = 0.5;
+        sol.Cr = 0.9;
+        sol.xFis.push_back(rand() * (XjU - XjL) + XjL);
+
+        
+
+        for (int j = 3; j < aminoacids.size(); j++) {
+            sol.xBetas.push_back(rand() * (XjU - XjL) + XjL);
+            sol.xFis.push_back(rand() * (XjU - XjL) + XjL);
+        }
+        population.push_back(sol);
+    }
+
+    // 
+
 
     return 0;
 }
+
